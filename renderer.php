fd99c42d35633326b3e8aeba3adcd7fb3484d216
Hittesh Ahuja <hitteshahuja@gmail.com>
2020-08-06 11:33:08 +0100

    phpdocs

--- renderer.php
+++ renderer.php
@@ -14,7 +14,9 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 defined('MOODLE_INTERNAL') || die();
-
+/*
+ * renderer for tool_hitteshahuja
+ */
 /**
  * Class file for renderer
  * @package   tool_hitteshahuja
